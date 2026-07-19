c74a0cc713d953428ca34defe48d643d9ad88834
shehla <shehs-at-tu-chemnitz.de>
2026-07-19 23:29:35 +0200

    sql testing expression

--- my_db.sql
+++ my_db.sql
@@ -1,11 +1,2 @@
-/*aliasing*/
-/*contraints*/
-/*calculation*/
-/*functions*/
-
-SELECT name, guild, level AS character_level,1 AS version,
-experience, experience/100,
-experience + 100/level *2,
-SQRT(16),SQRT(level),
-upper(guild)
-FROM my_db.characters;
+/*testing*/
+SELECT 'Hey',1,false
