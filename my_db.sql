04d38aa8a9c293ae1d07f1b38d54bd8c2f5366f2
shehla <shehs-at-tu-chemnitz.de>
2026-07-24 00:14:33 +0200

    logical negation

--- my_db.sql
+++ my_db.sql
@@ -1,4 +1,2 @@
-SELECT name, level, mentor_id, is_alive,class
-FROM my_db.characters 
-WHERE (level >20 AND is_alive = "true"  OR mentor_id IS NOT NULL) AND NOT (class IN( 'Mage', 'Archer'))
-
+SELECT NOT false //1
+SELECT NOT true //0
