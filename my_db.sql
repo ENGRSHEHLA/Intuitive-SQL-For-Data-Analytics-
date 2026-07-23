0a5ecab8158938c12a64e05b591ae195d0ec9002
shehla <shehs-at-tu-chemnitz.de>
2026-07-23 23:59:55 +0200

    filtering and removing class/column

--- my_db.sql
+++ my_db.sql
@@ -1,4 +1,4 @@
-SELECT name, level, level /10 AS level_scaled
+SELECT name, level, mentor_id, is_alive,class
 FROM my_db.characters 
-WHERE level /10 >3
+WHERE (level >20 AND is_alive = "true"  OR mentor_id IS NOT NULL) AND NOT (class IN( 'Mage', 'Archer'))
 
