997d8113b3e5220328dd96298a06e25916dc588a
shehla <shehs-at-tu-chemnitz.de>
2026-08-08 23:05:14 +0200

    Create a new table from the results of this SELECT query.

--- my_db.sql
+++ my_db.sql
@@ -1,1 +1,10 @@
-SELECT  DISTINCT class, guild FROM my_db.characters ORDER BY class, guild
+CREATE TABLE my_db.characters_alive
+AS
+SELECT *
+FROM  my_db.characters WHERE is_alive = true;
+
+CREATE TABLE my_db.characters_dead
+AS
+SELECT *
+FROM  my_db.characters WHERE is_alive = false
+;
