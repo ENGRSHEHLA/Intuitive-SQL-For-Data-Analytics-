19dd708ddb898f330bb9d2ab9ca78bf046abed40
shehla <shehs-at-tu-chemnitz.de>
2026-08-23 23:55:03 +0200

    UNION query and cast experience to CHAR

--- my_db.sql
+++ my_db.sql
@@ -1,7 +1,7 @@
-SELECT id, name, item_type, power, date_added
+SELECT id, name, item_type, power, date_added,rarity
 FROM my_db.items
 
 UNION DISTINCT
 
-SELECT id, name, class, level, last_active 
+SELECT id, name, class, level, last_active, CAST(experience AS char)
  FROM my_db.characters;
