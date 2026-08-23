13a212543c2a7f215354529d00a8728fd49e8d08
shehla <shehs-at-tu-chemnitz.de>
2026-08-23 23:45:55 +0200

    use of UNIONH DISTINCT in real image

--- my_db.sql
+++ my_db.sql
@@ -1,5 +1,7 @@
-SELECT name FROM my_db.characters
+SELECT id, name, item_type, power, date_added
+FROM my_db.items
 
 UNION DISTINCT
 
-SELECT name FROM my_db.items;
+SELECT id, name, class, level, last_active 
+ FROM my_db.characters;
