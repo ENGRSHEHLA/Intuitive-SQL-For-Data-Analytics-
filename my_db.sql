5e6e878e6db1f5ae542b8e24971a0e0890ac5b02
shehla <shehs-at-tu-chemnitz.de>
2026-09-11 16:08:47 +0200

    more easy case statement

--- my_db.sql
+++ my_db.sql
@@ -2,6 +2,6 @@ SELECT name, level, level>= 20 AS new_level,
 CASE
    WHEN level < 12 THEN 'low'
    WHEN level >= 15 AND level >25 THEN 'mid'
-   WHEN level >=25 THEN 'supper'
-  END AS level_category
+   ELSE 'super'
+  END AS level_bucket
  FROM my_db.characters WHERE is_alive = true 
