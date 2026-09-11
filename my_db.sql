0d67a1483f4868563cb0826f1df0fefce8f44119
shehla <shehs-at-tu-chemnitz.de>
2026-09-11 15:54:22 +0200

    case statement

--- my_db.sql
+++ my_db.sql
@@ -1,3 +1,7 @@
---Flag (level >= 20 AS new_level)
-SELECT name, level, level>= 20 AS new_level
+SELECT name, level, level>= 20 AS new_level,
+CASE
+   WHEN level < 12 THEN 'low'
+   WHEN level >= 15 AND level >25 THEN 'mid'
+   WHEN level >=25 THEN 'supper'
+END
  FROM my_db.characters WHERE is_alive = true ;
