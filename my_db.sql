7ae86a5bd59bf12cd88a20b4cbe353ed64ca08d0
shehla <shehs-at-tu-chemnitz.de>
2026-09-11 16:01:01 +0200

    Complete case statement

--- my_db.sql
+++ my_db.sql
@@ -3,5 +3,5 @@ CASE
    WHEN level < 12 THEN 'low'
    WHEN level >= 15 AND level >25 THEN 'mid'
    WHEN level >=25 THEN 'supper'
-END
- FROM my_db.characters WHERE is_alive = true ;
+  END AS level_category
+ FROM my_db.characters WHERE is_alive = true 
