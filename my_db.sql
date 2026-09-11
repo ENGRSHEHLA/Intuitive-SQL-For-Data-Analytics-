19daa386255235578a039e7dbe86f0ad1279cbb5
shehla <shehs-at-tu-chemnitz.de>
2026-09-11 16:42:50 +0200

    result of query by image

--- my_db.sql
+++ my_db.sql
@@ -1,7 +1,3 @@
-SELECT name, level, level>= 20 AS new_level,
-CASE
-   WHEN level < 12 THEN 'low'
-   WHEN level >= 15 AND level >25 THEN 'mid'
-   ELSE 'super'
-  END AS level_bucket
- FROM my_db.characters WHERE is_alive = true 
+SELECT sum(level), avg(level), min(level), max(level) AS max_level, COUNT(level),
+max(experience)  AS max_exp
+FROM my_db.characters
