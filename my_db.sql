b410b2d5342e3e5e1bb3b5bc80ab5f68e91ea4da
shehla <shehs-at-tu-chemnitz.de>
2026-09-17 00:22:45 +0200

    etrieve mentee and mentor experience( List all rows from characters who have a mentor, showing the mentee's ID, mentor's ID, mentee's experience, and mentor's experience.)

--- my_db.sql
+++ my_db.sql
@@ -1,1 +1,6 @@
-SELECT id,mentor_id, experience FROM my_db.characters where mentor_id is NOT null;
+SELECT id AS mentee_id, mentor_id, experience AS menteee_experience,
+( SELECT experience from my_db.characters AS mentor_table where id = mentee_table.mentor_id)
+  AS mentor_experience
+ FROM my_db.characters AS mentee_table where mentor_id is NOT null;
+
+
