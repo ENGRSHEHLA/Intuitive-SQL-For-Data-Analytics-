166edada4396cedbc0fca997318ae506564c9ad8
shehla <shehs-at-tu-chemnitz.de>
2026-09-16 23:13:45 +0200

    Use of sub query

--- my_db.sql
+++ my_db.sql
@@ -1,5 +1,2 @@
-
--- In PostgreSQL STRING_AGG(class, ",")
-
-SELECT count(class), min(class), max(class),GROUP_CONCAT(class SEPARATOR '!')
-FROM my_db.characters
+SELECT experience FROM characters where experience > ( select min(experience) FROM characters)
+;
