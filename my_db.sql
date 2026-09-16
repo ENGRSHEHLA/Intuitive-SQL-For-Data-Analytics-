240d4cbd57bb62bf13fd37b9505d3a27af394825
shehla <shehs-at-tu-chemnitz.de>
2026-09-16 23:23:55 +0200

    filter characters by experience range in sub query

--- my_db.sql
+++ my_db.sql
@@ -1,2 +1,5 @@
-SELECT experience FROM characters where experience > ( select min(experience) FROM characters)
+SELECT name,experience FROM my_db.characters where experience > 
+( select min(experience) FROM my_db.characters)
+AND experience < 
+( select max(experience) FROM my_db.characters)
 ;
